fbbd277394a1cec1cb12ec05cfde521b6199b3f1
Asklit <forgrace5@gmail.com>
2023-09-07 18:32:16 +0500

    new form

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.config['SECRET_KEY'] = 'hse_secret_key'
 @app.route('/', methods=["GET", "POST"])
 def main():
     form = FeedbackForm()
-    message = ['', '', '', '']
+    message = [' ', ' ', ' ', ' ']
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = User(
@@ -25,10 +25,7 @@ def main():
         )
         db_sess.add(user)
         db_sess.commit()
-        message = ["Спасибо вам за ваше сообщение, мы отправим вам ответ на почту",
-                   'Thank you for your message, we will send you a reply by email',
-                   'Gracias por su mensaje, le enviaremos una respuesta por correo',
-                   '感谢您的留言我们将通过电子邮件向您发送回复']
+        return redirect(f'/thanks')
     title = ['Отправьте нам сообщение', 'Send us a feedback', 'Envíenos un mensaje', '给我们留言']
     title2 = ['Отправьте нам сообщение', 'Send us a feedback', 'Envíenos un mensaje', '给我们留言']
     name = ['Имя', 'Name', 'Nombre', '姓名']
@@ -37,18 +34,29 @@ def main():
                   'Déjanos tu correo electrónico para comentarios', '留下您的电子邮件以获取反馈']
     send = ['Отправить', 'Send', 'Enviar', '发送']
     choose = ['Выбор языка', 'Language selection', 'Selección de idioma', '语言选择']
-    russian_language = ['Русский', 'Russian', 'Rusa', '俄语']
-    english_language = ['Английский', 'English', 'Inglés', '英语']
-    spanish_language = ['Испанский', 'Spanish', 'Español', '西班牙语']
-    chinese_language = ['Китайский', 'Chinese', 'Chino', '中文']
     made_by = ['Сделано студентами Высшей Школы Экономики', 'Made by students of the Higher School of Economics',
                'Hecho por estudiantes de la escuela Superior de Economía', '由高等经济学院的学生制作']
     return render_template('base.html', form=form, title=title[ID_LANGUAGE], message=message[ID_LANGUAGE], ID_LANGUAGE=ID_LANGUAGE,
                            title2=title2[ID_LANGUAGE], name=name[ID_LANGUAGE], text=text[ID_LANGUAGE],
-                           email_form=email_form[ID_LANGUAGE], russian_language=russian_language[ID_LANGUAGE],
-                           english_language=english_language[ID_LANGUAGE], spanish_language=spanish_language[ID_LANGUAGE],
-                           chinese_language=chinese_language[ID_LANGUAGE], send=send[ID_LANGUAGE], choose=choose[ID_LANGUAGE],
+                           email_form=email_form[ID_LANGUAGE], send=send[ID_LANGUAGE], choose=choose[ID_LANGUAGE],
                            made_by=made_by[ID_LANGUAGE])
+
+
+@app.route('/thanks', methods=["GET", "POST"])
+def thanks():
+    message = ["Спасибо вам за ваше сообщение",
+                   'Thank you for your message',
+                   'Gracias por su mensaje',
+                   '谢谢您的留言']
+    choose = ['Выбор языка', 'Language selection', 'Selección de idioma', '语言选择']
+    text = ['Мы получили ваше сообщение. Мы отправим вам ответ на почту в ближайшее время',
+            'We have received your message. We will send you a reply by email as soon as possible.',
+            'Hemos recibido su mensaje. Le enviaremos una respuesta por correo electrónico pronto.',
+            '我们已经收到你的信息 我们将尽快通过电子邮件向您发送回复']
+    made_by = ['Сделано студентами Высшей Школы Экономики', 'Made by students of the Higher School of Economics',
+               'Hecho por estudiantes de la escuela Superior de Economía', '由高等经济学院的学生制作']
+    return render_template('thanks.html', choose=choose[ID_LANGUAGE],
+                           made_by=made_by[ID_LANGUAGE], message=message[ID_LANGUAGE], text=text[ID_LANGUAGE])
 
 
 @app.route('/russian', methods=["GET", "POST"])
